998d30393342f52cf310c100c4efcd1ca0b96750
Kouda <kdark@asia.com>
2023-04-16 15:23:17 +0100

    Changed optional laptop to have an unlocked cursor

--- FoundationMathGame/Assets/Scripts/InteractScreen.cs
+++ FoundationMathGame/Assets/Scripts/InteractScreen.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Unity.VisualScripting;
-using UnityEditor.Tilemaps;
 
 // Derived from Advanced Games Design 'P.E.T.S Zoo Simulator code pcController.cs'
 public class InteractScreen : MonoBehaviour
@@ -11,8 +9,6 @@ public class InteractScreen : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject pcCam;
     [SerializeField] private GameObject playerCvs;
-    private Coroutine zoomCoroutine;
-//    private float counter = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -27,19 +23,17 @@ public class InteractScreen : MonoBehaviour
             // 'E' to Enter the Interaction Screen
             if (Input.GetKey(KeyCode.E))
             {
-            //    counter = 0;
                 playerCvs.SetActive(false);
                 player.GetComponent<FirstPersonController>().enabled = false;
                 player.GetComponentInChildren<Camera>().enabled = false;
                 pcCam.GetComponent<Camera>().enabled = true;
-            
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+
             }
             // 'F' to Finish using the Interaction Screen
             else if (Input.GetKey(KeyCode.F)) 
             {
-                if (zoomCoroutine != null)
-                    StopCoroutine(zoomCoroutine);
-//                pcCam.GetComponent<Camera>().fieldOfView = 60;
                 playerCvs.SetActive(true);
                 player.GetComponent<FirstPersonController>().enabled = true;
                 player.GetComponentInChildren<Camera>().enabled = true;
