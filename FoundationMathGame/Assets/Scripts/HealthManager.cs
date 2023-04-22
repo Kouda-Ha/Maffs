1d4eff2e9df41526c682aa2259db36367120e481
Kouda <kdark@asia.com>
2023-04-22 21:13:24 +0100

    HP displays and shows remaining hp

--- FoundationMathGame/Assets/Scripts/HealthManager.cs
+++ FoundationMathGame/Assets/Scripts/HealthManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.XR.LegacyInputHelpers;
+using UnityEngine.UI;
+using TMPro;
 
 public class HealthManager : MonoBehaviour
 {
     public int health;
     public int damaged;
+    [SerializeField] private TMP_Text healthText;
 
     public void TakeDamage(int amount)
     {
         health -= amount;
+        healthText.SetText("Health: " + health + "%");
     }
 
     public void DealDamage(GameObject target)
